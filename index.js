56840fb6c4532a0a400149352879fb1e28efb7ba
mulukimwangeka <mwangekaruth@gmail.com>
2022-08-31 16:54:03 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+document.addEventListener('DOMContentLoaded', fetchBooks);
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(res=>res.json())
+  .then(data =>{
+    renderBooks(data);
+  })
   
 }
 
 function renderBooks(books) {
+    
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
@@ -12,6 +21,5 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+
